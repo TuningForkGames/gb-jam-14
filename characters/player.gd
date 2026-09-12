497a14f54e2fe45f6f7cac7d385cb2639ade30b5
Johnmichael Nichols <halt@Johnmichaels-MacBook-Pro.local>
2026-09-12 15:45:15 -0400

    added base line character movement and init of player scne

---
+++ characters/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
