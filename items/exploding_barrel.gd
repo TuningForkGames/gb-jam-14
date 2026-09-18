d43e2ba498aaca3889c6d106b3378bc1def9432f
halt <46973377+johnmichaeldnichols@users.noreply.github.com>
2026-09-17 23:40:55 -0400

    interaction reparenting working, weight added, next adding player use effects

--- items/exploding_barrel.gd
+++ items/exploding_barrel.gd
@@ -3,12 +3,17 @@ extends Area2D
 @onready var interactable: Area2D = $Interactable
 @onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var _player
 
 func _ready() -> void:
 	interactable.interact = _on_interact
+	interactable.weight = Interactable.weight.heavy
+	_player = get_tree().current_scene.find_child("Player", true, false)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_interact():
+	self.reparent(_player)
+	self.position = Vector2(0,-14)
+	self.z_index = 3
 	if sprite_2d.frame == 0:
 		sprite_2d.frame = 1
 		interactable.is_interactable = false
--- scripts/interaction/interactable.gd
+++ scripts/interaction/interactable.gd
@@ -1,7 +1,10 @@
+class_name Interactable
 extends Area2D
 
 @export var interact_name: String = ""
 @export var is_interactable: bool = true
+enum weight {light, normal, heavy}
+@export var interactable_weight:weight = weight.normal
 
 var interact: Callable = func():
 	pass
